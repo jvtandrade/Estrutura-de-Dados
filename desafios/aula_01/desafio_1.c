91eb7de1ad6b3e096bfb4a00bc72c9fb436bf534
jvtandrade <152717876+jvtandrade@users.noreply.github.com>
2026-03-04 11:48:56 -0300

    resolução dos desafios da aula 1

---
+++ desafios/aula_01/desafio_1.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+struct no {
+    int info;         
+    struct no * prox;
+};
+
+struct no * novoNo(int info) {
+    struct no * novo = malloc(sizeof(struct no));
+    novo->info = info;
+    return novo;
+}
+
+struct no * insert_first(struct no * lista, int info) {
+    struct no *novo = novoNo(info);
+    if (!novo) return lista;   // se falhar, mantém a lista como estava
+    novo->prox = lista;        // novo aponta para a antiga cabeça
+    return novo;               // novo vira a cabeça
+}
+
+void insert_last(struct no * lista, int info) {
+    struct no *novo = novoNo(info);
+    // Precisamos encontrar o último elemento da lista. Quando encontramos, adicionamos o novo!
+    struct no *curr = lista;
+    while (curr->prox != NULL) {
+        curr = curr->prox;
+    }
+    curr->prox = novo;
+}
+
+struct no * remove_first(struct no * lista) {
+    if (lista == NULL) return NULL; // lista vazia
+
+    struct no *novo_inicio = lista->prox;
+    return novo_inicio;
+}
+
+void printList (struct no * lista) {
+    for (struct no * curr = lista; curr != NULL; curr = curr->prox) {
+        printf("%d", curr->info);
+    }
+}
+
+int main() {
+    struct no * lista = novoNo(1);
+    lista = insert_first(lista, 2);
+    lista = insert_first(lista, 3);
+    lista = insert_first(lista, 4);
+
+    printList(lista);
+    return 0;
+}
